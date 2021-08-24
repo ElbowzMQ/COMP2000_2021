df92bc4d1a566847319197db6aa01b5d0894dd10
ElbowzMQ <joshua.aspinall@students.mq.edu.au>
2021-08-24 18:04:58 +1000

    week 5

--- src/Grid.java
+++ src/Grid.java
@@ -1,37 +1,53 @@
-import java.awt.*;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.util.Optional;
 
-public class Grid {
-    Cell [][] cells;
-    int x = 10;
-    int y = 35;
-    
-    
-    public Grid(){
-        cells = new Cell [20][20];
-        
-        for(int i=0; i < cells.length; i++){            
-            if(i != 0){
-                x += 35;
-            }                                       
-            for(int j=0; j < cells[i].length; j++){
-                if(j == 0){
-                    y = 35;
-                } else {
-                    y += 35;
-                }
-                cells[i][j] = new Cell(x,y);              
+class Grid {
+    Cell[][] cells = new Cell[20][20];
+
+    public Grid() {
+        for(int i = 0; i < cells.length; i++) {
+            for(int j = 0; j < cells[i].length; j++) {
+                cells[i][j] = new Cell(colToLabel(i), j, 10+35*i, 10+35*j);
             }
         }
     }
-    
-    public void paint(Graphics g){
-        for(int i = 0; i < cells.length; i++){
-            for(int j = 0; j < cells[i].length; j++){
-                g.setColor(Color.WHITE);
-                g.fillRect(cells[i][j].x, cells[i][j].y, 35, 35);
-                g.setColor(Color.BLACK);
-                g.drawRect(cells[i][j].x, cells[i][j].y, 35, 35);
+
+    private char colToLabel(int col) {
+        return (char) (col + 65);
+    }
+
+    private int labelToCol(char col) {
+        return (int) col - 65;
+    }
+
+    public void paint(Graphics g, Point mousePos) {
+        for(int i = 0; i < cells.length; i++) {
+            for(int j = 0; j < cells[i].length; j++) { 
+                cells[i][j].paint(g, mousePos);
             }
         }
+    }
+
+    private Optional<Cell> cellAtColRow(int c, int r) {
+        if(c >= 0 && c < cells.length && r >= 0 && r < cells[c].length) {
+            return Optional.of(cells[c][r]);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<Cell> cellAtColRow(char c, int r) {
+        return cellAtColRow(labelToCol(c), r);
+    }
+    public Optional<Cell> cellAtPoint(Point p) {
+        for(int i=0; i < cells.length; i++) {
+            for(int j=0; j < cells[i].length; j++) {
+                if(cells[i][j].contains(p)) {
+                    return Optional.of(cells[i][j]);
+                }
+            }
+        }
+        return Optional.empty();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
-public class Main {
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+class Main extends JFrame {
+    
+    class App extends JPanel {
+        
+        Stage stage;
+
+        public App() {
+            setPreferredSize(new Dimension(1024, 720));
+            stage = new Stage();
+            stage = StageReader.readStage("data/stage1.rvb");
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            stage.paint(g, getMousePosition());
+        }
+
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Red vs. Blue");
+        Main window = new Main();
+        window.run();
+    }
+
+    private Main() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        App canvas = new App();
+        this.setContentPane(canvas);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    public void run() {
+        while (true) {
+            this.repaint();
+        }
     }
 }
